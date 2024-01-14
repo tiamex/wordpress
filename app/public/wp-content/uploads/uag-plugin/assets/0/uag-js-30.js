8d159f8f815c79f475c62298e435de060f8b9263
tiamex <mattia.messina993@gmail.com>
2024-01-14 17:14:42 +0100

    astra theme

---
+++ app/public/wp-content/uploads/uag-plugin/assets/0/uag-js-30.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-0e08d916', {"layout":"number","heading":"Presenters","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":10,"totalNumber":100,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-88f79663', {"layout":"number","heading":"Attendees","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":100,"totalNumber":100,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-096eefd6', {"layout":"number","heading":"Places","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":10,"totalNumber":100,"decimalPlaces":0,"animationDuration":2000,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
